0b980a14bbe71750b7bf34494a209329e6877742
Toshiyuki Sugimoto <osugi.to.pico@gmail.com>
2012-05-01 14:23:00 +0900

    error: railsのバージョンによるかもしれない。

--- p4u/db/migrate/20120426035954_deleteuniqeindex.rb
+++ p4u/db/migrate/20120426035954_deleteuniqeindex.rb
@@ -1,6 +1,6 @@
 class Deleteuniqeindex < ActiveRecord::Migration
   def up
-    add_index :individual_kos, :player_id, :character_id
+    add_index :individual_kos, [:player_id, :character_id]
   end
 
   def down
